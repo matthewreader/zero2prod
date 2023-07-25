703c4b597e90b4d4a7fc2caaf0758a84b8d2376d
matthewreader <mdreader@gmail.com>
2023-07-25 07:14:26 -0500

    Create health_check.rs and lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use std::net::TcpListener;
+use zero2prod::run;
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let address = TcpListener::bind("127.0.0.1:8000");
+    run(address.unwrap())?.await
 }
